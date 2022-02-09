232edd2f71f226a759f38804a056a22df9928ff5
FDR <delrincon@gmail.com>
2022-02-09 20:03:41 -0300

    detailsScreen - picture title and shadows done

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -32,7 +32,9 @@ const MoviePoster = ({ movie, height = 420, width = 300 }:Props) => {
             style={{
                 width,
                 height,
-                marginHorizontal: 8,
+                marginHorizontal: 2,
+                paddingBottom: 20,
+                paddingHorizontal: 7,
             }}
         >
             <View style={styles.imageContainer} >
--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,22 +1,75 @@
 import React from 'react';
 import { StackScreenProps } from '@react-navigation/stack';
-import { Text, View } from 'react-native';
+import { Dimensions, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { RootStackParams } from '../navigation/Navigation';
 
-interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'>{
 
-}
+const screenHeight = Dimensions.get('screen').height;
+
+
+interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'>{}
 
 
 export const DetailScreen = ( { route }:Props ) => {
 
     const movie = route.params;
+    const uri = `https://image.tmdb.org/t/p/w500${ movie.poster_path }`;
 
-    console.log(movie.original_title)
+
 
     return (
-        <View>
-            <Text>Detail - Screen</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.imageContainer}>
+                <Image 
+                    source={{ uri }}
+                    style={styles.posterImage}
+                />
+            </View>
+
+            <View style={styles.marginContainer}>
+                <Text style={styles.subTitle}>{movie.original_title}</Text>
+                <Text style={styles.title}>{movie.title}</Text>
+            </View>
+
+        </ScrollView>
     )
 };
+
+const styles = StyleSheet.create({
+    imageContainer : {
+        overflow: 'hidden',
+        width: '100%',
+        height: screenHeight * 0.7,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 10,
+        },
+        shadowOpacity: 0.24,
+        shadowRadius: 4.65,
+
+        elevation: 10,
+
+        borderBottomEndRadius: 25,
+        borderBottomStartRadius: 25,
+        // borderBottomLeftRadius: 25,
+        // borderBottomRightRadius: 25,
+    },
+    posterImage : {
+        flex: 1,
+        // borderBottomEndRadius: 25,
+    },
+
+    marginContainer: {
+        marginHorizontal: 20,
+        marginTop: 20,
+    },
+    subTitle: {
+        fontSize: 16,
+        opacity: 0.8,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+});
